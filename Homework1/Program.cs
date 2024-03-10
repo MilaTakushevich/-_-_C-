e2d0857b38dd149857eb7db2949ccf5b2758c4a3
Mila <marina.vtalians@gmail.com>
2024-03-10 20:41:28 +0300

    создали папку homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите второе число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("Первое больше второго");
+}
+if (number1 > number2)
+{
+    Console.WriteLine("Второе больше первого");
+}
+else
+{
+    Console.WriteLine("числа равны");
+}
