d09cddcb9481bfe35c90cbf85da4e18bd18ae018
Mila <marina.vtalians@gmail.com>
2024-03-11 00:04:36 +0300

    решение всех 4 задач готово

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,43 +1,59 @@
-﻿//Console.WriteLine("Введите число: ");
-//int number1 = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Введите второе число: ");
-//int number2 = Convert.ToInt32(Console.ReadLine());
-//if (number1 > number2)
-//{
-//    Console.WriteLine("Первое больше второго");
-//}
-//else if (number1 > number2)
-//{
-//    Console.WriteLine("Второе больше первого");
-//}
+﻿// напишите метод CompareNumbers, который на вход принимает два числа и выводит, какое число большее, а какое меньшее.
 
-//else
-//{
-//    Console.WriteLine("числа равны");
-//}
+// Введите свое решение ниже
 
-
-
-Console.WriteLine("Введите число: ");
-//Считываем введенное пользователем число и преобразуем его в целочисленный тип
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число: ");
-//Считываем введенное пользователем число и преобразуем его в целочисленный тип
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число: ");
-//Считываем введенное пользователем число и преобразуем его в целочисленный тип
-int c = Convert.ToInt32(Console.ReadLine());
-
-int a == max;
-
-if (b > max)
+if (firstNumber < secondNumber)
 {
-int b == max;
+Console.WriteLine("второе число больше");
+}
+else if (firstNumber > secondNumber)
+{
+Console.WriteLine("первое число больше");
+}
+else
+{
+Console.WriteLine("числа равны");
 }
 
-if (c < max)
-{
-int c == max;
-}
 
-Console.WriteLine("max");
+// напишите метод FindMax, который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Введите свое решение ниже 
+
+ int max = a; 
+        
+ if (b > max)
+ {
+   max = b;
+ }
+
+ if (c > max)
+ {
+   max = c;
+ }
+
+ return max;
+
+
+// напишите метод CheckIfEven, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+
+ // Введите свое решение ниже
+if (number % 2 == 0)
+    {
+        Console.WriteLine("четное");
+    }
+    else
+    {
+        Console.WriteLine("нечетное");
+    }
+
+// напишите метод PrintEvenNumbers, которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), после каждого числа должен быть знак пробела.
+
+// Введите свое решение ниже
+for (int i = 2; i <= number; i += 2)
+        {
+            Console.Write(i + " ");
+
+    }
+
+
