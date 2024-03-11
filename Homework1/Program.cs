3b4e9cbe1ae9b8aea9ec3723d50b309926eb19b3
Mila <marina.vtalians@gmail.com>
2024-03-11 15:48:40 +0300

    почистили файл с дз

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -2,58 +2,58 @@
 
 // Введите свое решение ниже
 
-if (firstNumber < secondNumber)
-{
-Console.WriteLine("второе число больше");
-}
-else if (firstNumber > secondNumber)
-{
-Console.WriteLine("первое число больше");
-}
-else
-{
-Console.WriteLine("числа равны");
-}
+//if (firstNumber < secondNumber)
+//{
+//Console.WriteLine("второе число больше");
+//}
+//else if (firstNumber > secondNumber)
+//{
+//Console.WriteLine("первое число больше");
+//}
+//else
+//{
+//Console.WriteLine("числа равны");
+//}
 
 
 // напишите метод FindMax, который принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 // Введите свое решение ниже 
 
- int max = a; 
+ //int max = a; 
         
- if (b > max)
- {
-   max = b;
- }
+ //if (b > max)
+ //{
+ //  max = b;
+ //}
 
- if (c > max)
- {
-   max = c;
- }
+ //if (c > max)
+ //{
+ //  max = c;
+ //}
 
- return max;
+ //return max;
 
 
 // напишите метод CheckIfEven, который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
 
  // Введите свое решение ниже
-if (number % 2 == 0)
-    {
-        Console.WriteLine("четное");
-    }
-    else
-    {
-        Console.WriteLine("нечетное");
-    }
+//if (number % 2 == 0)
+//    {
+//        Console.WriteLine("четное");
+//    }
+//    else
+//    {
+//        Console.WriteLine("нечетное");
+//    }
 
 // напишите метод PrintEvenNumbers, которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), после каждого числа должен быть знак пробела.
 
 // Введите свое решение ниже
-for (int i = 2; i <= number; i += 2)
-        {
-            Console.Write(i + " ");
+//for (int i = 2; i <= number; i += 2)
+//        {
+//            Console.Write(i + " ");
 
-    }
+//    }
 
 
