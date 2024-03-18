7481d69ef328fd203dfc704ecafb6ebd66b38cb4
Mila <marina.vtalians@gmail.com>
2024-03-18 08:40:06 +0300

    задача с выводом чисел через запятую

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,58 +1,55 @@
-﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+﻿/* Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 
-//Console.WriteLine("Ведите число: ");
-//int number = Convert.ToInt32(Console.ReadLine());
-//if(number %7 == 0 && number %23 == 0)
-//{
-//    Console.WriteLine("да");
-//}
-//else
-//{
-//    Console.WriteLine("нет");
-//}
+Console.WriteLine("Ведите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number %7 == 0 && number %23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
 
 // Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
 
-//Console.WriteLine("Ведите координату х (х не равно 0): ");
-//int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите координату х (х не равно 0): ");
+int x = Convert.ToInt32(Console.ReadLine());
 
-//Console.WriteLine("Ведите координату у (у не равно 0): ");
-//int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите координату у (у не равно 0): ");
+int y = Convert.ToInt32(Console.ReadLine());
 
-//if(x > 0 && y > 0)
-//{
-//    Console.WriteLine("Первый номер координатной четверти плоскости");
-//}
-//else if(x < 0 && y > 0)
-//{
-//    Console.WriteLine("Второй номер координатной четверти плоскости");
-//}
-//else if(x < 0 && y < 0)
-//{
-//    Console.WriteLine("Третий номер координатной четверти плоскости");
-//}
-//else if(x > 0 && y < 0)
-//{
-//    Console.WriteLine("Четвертый номер координатной четверти плоскости");
-//}
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("Первый номер координатной четверти плоскости");
+}
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("Второй номер координатной четверти плоскости");
+}
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("Третий номер координатной четверти плоскости");
+}
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Четвертый номер координатной четверти плоскости");
+}
 
 // Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
-
-
-// Выводим приглашение для ввода двузначного числа
-//        Console.WriteLine("Введите двузначное число: ");
-// Считываем введенное пользователем число и преобразуем его в целое число
-//int number = Convert.ToInt32(Console.ReadLine());
+ Выводим приглашение для ввода двузначного числа
+Console.WriteLine("Введите двузначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
         
 // Проверяем, является ли введенное число двузначным
-/*        if (number > 10 && number < 100)
-        {
-            // Разбиваем число на десятки и единицы
-            int dec = number / 10; // десятки
-            int ed = number % 10;  // единицы
+if (number > 10 && number < 100)
+    {
+// Разбиваем число на десятки и единицы
+        int dec = number / 10; // десятки
+        int ed = number % 10;  // единицы
             
-            // Проверяем, какая цифра больше и выводим ее
-            if (dec > ed)
+// Проверяем, какая цифра больше и выводим ее
+        if (dec > ed)
             {
                 Console.WriteLine(dec);
             }
@@ -60,42 +57,22 @@
             {
                 Console.WriteLine(ed);
             }
-        }
-        else
-        {
-            // Если число не двузначное, выводим сообщение об ошибке
-            Console.WriteLine("Ваше число не двузначное");
-        }
+    }
+else
+    {
+// Если число не двузначное, выводим сообщение об ошибке
+        Console.WriteLine("Ваше число не двузначное");
+    }
 
-// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.    
 */
-//Console.WriteLine("Введите натуральное число:");
- //       int number = Convert.ToInt32(Console.ReadLine());
 
-        // Проверяем, является ли введенное значение числом, и оно больше 0
-       /* while (!int.TryParse(Console.ReadLine(), out number) || number <= 0)
-        {
-            Console.WriteLine("Ошибка. Введите натуральное число:");
-        }
-
-        // Переводим число в строку для удобства работы с ним посимвольно
-        string numberStr = number.ToString();
-
-        // Выводим первую цифру
-        Console.Write(numberStr[0]);
-
-        // Выводим остальные цифры с запятыми
-        for (int i = 1; i < numberStr.Length; i++)
-        {
-            Console.Write(", " + numberStr[i]);
-        }
-    */
-
-    Console.WriteLine("Введите натуральное число:");
-        int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
         string numberStr = number.ToString();
         Console.Write(numberStr[0]);
         for (int i = 1; i < numberStr.Length; i++)
         {
             Console.Write(", " + numberStr[i]);
         }
+        
---
+++ practic3/Program.cs
@@ -0,0 +1,102 @@
+﻿/* Напишите программу, которая определяет,
+присутствует ли заданное число в массиве. Программа
+должна выдать ответ: Да/Нет.
+Примеры
+[1 3 4 19 3], 8 => Нет
+[-4 3 4 1], 3 => Да
+
+int[] array = { 1, 3, 4, 19, 3 };
+Console.Write("Введите число для поиска: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int v = 0;
+
+foreach (int e in array)
+{
+    if (e == x) { v = x; }
+}
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == x) { v = x; }
+}
+
+if (v == x)
+{ Console.Write("Да"); }
+else
+{
+    Console.Write("Нет");
+}
+
+Задайте массив из 10 элементов, заполненный числами из
+промежутка [-10, 10]. Замените отрицательные элементы на
+положительные, а положительные на отрицательные.
+Пример
+[1 -5 6]
+=> [-1 5 -6]
+
+int[] arr = new int[10];
+for (int i = 0; i < 10 ; i++)
+{
+    arr[i] = new Random().Next (-10, 11);
+    Console.Write($"arr [{i}] = {arr[i]} ; ");
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < 10 ; i++)
+{
+    arr[i] = arr[i] * -1;
+    Console.Write($"{arr[i]} ");
+}
+
+Найдите произведения пар чисел в одномерном массиве. Парой
+считаем первый и последний элемент, второй и предпоследний и
+т.д. Результат запишите в новый массив.
+Пример
+[1 3 2 4 2 3] => [3 6 8]
+[2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
+
+
+int r = new Random().Next (1, 11);
+int[] arr = new int[r];
+for (int i = 0; i < arr.Length ; i++)
+{
+    arr[i] = new Random().Next (1, 11);
+    Console.Write($"{arr[i]} ");
+}
+System.Console.WriteLine();
+
+int r = new Random().Next (1, 11);
+int[] arr = new int[r];
+for (int i = 0; i < arr.Length ; i++)
+{
+    arr[i] = new Random().Next (1, 11);
+    Console.Write($"{arr[i]} ");
+}
+System.Console.WriteLine();
+
+int[] arr2 = new int[r/2];
+for (int i = 0; i < arr.Length/2 ; i++)
+{
+    arr2[i] = arr[i] * arr[arr.Length-i-1];
+    Console.Write($" {arr2[i]} ");
+}
+
+Дано натуральное трёхзначное число. Создайте массив, состоящий из
+цифр этого числа. Младший разряд числа должен располагаться на 0-
+м индексе массива, старший – на 2-м.
+Пример
+456 => [6 5 4]
+781 => [1 8 7]
+
+
+int n = new Random().Next (100,1000);
+Console.Write(n);
+int[] arr3 = new int[3];
+for (int i = 0; i < arr3.Length ; i++)
+{
+    arr3[i] = n % 10;
+    Console.Write($" {arr3[i]} ");
+    n = n/10;
+}
+*/
