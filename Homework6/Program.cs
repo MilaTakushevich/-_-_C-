7af9eff143e9066defa72d3d3dc164de0928e0e4
Mila <marina.vtalians@gmail.com>
2024-04-03 20:49:02 +0300

    задача с формирование строки из массива С первым апреля!

---
+++ Homework6/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[,] charArray = {
+        {'C', ' ', 'п', 'е', 'р', 'в', 'ы', 'м'},
+        {' ', 'а', 'п', 'р', 'е', 'л', 'я', '!'}
+        };
+
+string resultString = GetStringFromArray(charArray);
+
+Console.WriteLine("Строка, созданная из символов массива:");
+Console.WriteLine(resultString);
+
+string GetStringFromArray(char[,] array)
+{
+    string result = ""; // Создаем пустую строку
+
+    for (int i = 0; i < array.GetLength(0); i++) // Проходим по всем элементам массива
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result += array[i, j];
+        }
+    }
+
+    return result;
+}
+
